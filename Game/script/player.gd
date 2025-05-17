08f9446029b61fb9c7db9fee8705aa56ab61873e
pupungeggang2 <kyongkyong2892@gmail.com>
2025-05-18 02:59:19 +0900

    Start

--- Game/script/player.gd
+++ Game/script/player.gd
@@ -2,3 +2,8 @@ extends Node
 
 var dungeon_floor = 0
 var dungeon_column = 1
+
+var level = 0
+var gold = 0
+var exp = 0
+var exp_max = 0
---
+++ Game/script/thing/player_battle.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+var hp = 60
+var hp_max = 60
+var energy = 3
+var energy_max = 6
+var deck = []
+var card = []
+var status = []
+
+func _ready() -> void:
+    pass
+    
+func _process(delta: float) -> void:
+    pass
