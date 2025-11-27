a152b84591682a87f9a6b8cb408ea660a0800d80
pupungeggang2 <kyongkyong2892@gmail.com>
2025-11-27 16:09:46 +0900

    Reset

--- Game/script/const.gd
+++
@@ -1,5 +0,0 @@
-extends Node
-
-const map_element_list = [
-    '', 'Start', 'Battle', 'Event', 'Shop', 'Rest', 'Boss'
-]
--- Game/script/thing/card.gd
+++
@@ -1,6 +0,0 @@
-extends Control
-
-func show_data(card):
-    get_node('TextEnergy').text = '%d' % [card.energy]
-    get_node('TextName').text = card.name
-    get_node('TextDescription').text = card.description
